0f6e8c590a4c5bc95e86c56a1794412b45c908d6
John Parker <jparker@urgetopunt.com>
2015-08-04 01:15:46 -0700

    make tests more deterministic to pass on rails 4.1

--- test/helpers/booties/application_helper_test.rb
+++ test/helpers/booties/application_helper_test.rb
@@ -4,22 +4,30 @@ require 'minitest/mock'
 module Booties
   class ApplicationHelperTest < ActionView::TestCase
     test '#tooltip renders span tag with given title' do
-      expected = '<span data-toggle="tooltip" title="foo bar">baz</span>'
+      expected = content_tag :span, 'baz',
+        data: { toggle: 'tooltip' }, title: 'foo bar'
+
       assert_equal expected, tooltip(title: 'foo bar') { 'baz' }
     end
 
     test '#tooltip with bottom placement' do
-      expected = '<span data-toggle="tooltip" data-placement="bottom" title="foo bar">baz</span>'
+      expected = content_tag :span, 'baz',
+        data: { toggle: 'tooltip', placement: 'bottom' }, title: 'foo bar'
+
       assert_equal expected, tooltip(title: 'foo bar', placement: :bottom) { 'baz' }
     end
 
     test '#tooltip with right placement' do
-      expected = '<span data-toggle="tooltip" data-placement="right" title="foo bar">baz</span>'
+      expected = content_tag :span, 'baz',
+        data: { toggle: 'tooltip', placement: 'right' }, title: 'foo bar'
+
       assert_equal expected, tooltip(title: 'foo bar', placement: :right) { 'baz' }
     end
 
     test '#tooltip with left placement' do
-      expected = '<span data-toggle="tooltip" data-placement="left" title="foo bar">baz</span>'
+      expected = content_tag :span, 'baz',
+        data: { toggle: 'tooltip', placement: 'left' }, title: 'foo bar'
+
       assert_equal expected, tooltip(title: 'foo bar', placement: :left) { 'baz' }
     end
 
@@ -32,7 +40,9 @@ module Booties
     end
 
     test '#tooltip accepts additional html attributes' do
-      expected = '<span data-toggle="tooltip" title="foo" class="bar">baz</span>'
+      expected = content_tag :span, 'baz',
+        data: { toggle: 'tooltip' }, title: 'foo', class: 'bar'
+
       assert_equal expected, tooltip(title: 'foo', class: 'bar') { 'baz' }
     end
 
